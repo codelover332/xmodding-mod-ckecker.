28c7fa9bcbb3e411aa588b329370c122d6dbfdc6
codelover332 <b6003368@gmail.com>
2026-02-06 22:25:45 -0500

    Add files via upload

---
+++ XMODDING-1.0.0/XMODDING-1.0.0/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using LibrePad.Patches;
+using LibrePad.Utilities;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace XMODDIGN
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Configuration Configuration;
+        public static bool Outdated;
+
+        void CheckVersion()
+        {
+            WebRequest request = WebRequest.Create("https://raw.githubusercontent.com/iiDk-the-actual/LibrePad/refs/heads/master/PluginInfo.cs");
+            WebResponse response = request.GetResponse();
+            Stream data = response.GetResponseStream();
+
+            if (data == null)
+                return;
+            
+            using StreamReader sr = new StreamReader(data);
+            string html = sr.ReadToEnd();
+
+            var match = Regex.Match(html, @"public const string Version\s*=\s*""([^""]+)"";");
+            if (!match.Success)
+                return;
+
+            string version = match.Groups[1].Value;
+            if (PluginInfo.Version != version)
+                Outdated = true;
+        }
+
+        void Awake()
+        {
+            CheckVersion();
+
+            GameObject loader = new GameObject("LibrePad");
+            DontDestroyOnLoad(loader);
+
+            Configuration = new Configuration(Config);
+            PatchHandler.PatchAll();
+            loader.AddComponent<Handler>();
+        }
+    }
+}
